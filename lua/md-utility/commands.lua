e9637eff4c26ad98ae2a8530084a2269ae2fde4e
Jaehaks <wogkr3654@naver.com>
2025-09-28 12:01:14 +0900

    fix : add wrapper function to autolist functions for easy usage.

--- lua/md-utility/commands.lua
+++ lua/md-utility/commands.lua
@@ -17,20 +17,44 @@ M.clipboard_paste = function(style)
 	Paste.ClipboardPaste(style)
 end
 
-M.autolist_cr = function (show_marker)
+M.autolist_cr_raw = function (show_marker)
 	return Autolist.autolist_cr(show_marker)
 end
 
-M.autolist_o = function (show_marker)
+M.autolist_o_raw = function (show_marker)
 	return Autolist.autolist_o(show_marker)
 end
 
-M.autolist_tab = function (reverse)
+M.autolist_tab_raw = function (reverse)
 	return Autolist.autolist_tab(reverse)
 end
 
 M.autolist_recalculate = function ()
 	return Autolist.autolist_recalculate()
+end
+
+-- wrapper to easy use for autolist_cr_raw
+M.autolist_cr = function (show_marker)
+	local autolist_cr = M.autolist_cr_raw(show_marker)
+	if not autolist_cr then
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", false)
+	end
+end
+
+-- wrapper to easy use for autolist_o_raw
+M.autolist_o = function (show_marker)
+	local autolist_o = M.autolist_o_raw(show_marker)
+	if not autolist_o then
+		vim.api.nvim_feedkeys('o', "n", false)
+	end
+end
+
+-- wrapper to easy use for autolist_tab_raw
+M.autolist_tab = function (reverse)
+	local autolist_tab = M.autolist_tab_raw(reverse)
+	if not autolist_tab then
+		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<TAB>", true, false, true), "n", false)
+	end
 end
 
 
